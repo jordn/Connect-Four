9d8baf50f49f244f985530c8a6341d0b8913c177
Jordan Burgess <jordanburgess@gmail.com>
2014-05-01 18:03:58 +0100

    tracks all possible lines, doesnt declare winner

--- connect4.py
+++ connect4.py
@@ -1,16 +1,10 @@
-import sys 
+###################
+#  CONNECT FOUR   #
+#  Jordan Burgess #
+#  2014-05-01     #
+#  MIT Licence    #
+###################
 
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-
-
-# print bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC
 
 PLAYER_TOKENS = ['X', 'O']
 
@@ -20,33 +14,75 @@ class Board(object):
         self.cols = cols
         self.rows = rows
         self.grid = [['.']*self.rows for c in xrange(self.cols)]
-        self.grid[0][5] = 'R'
-        self.player = 1 #whos turn
+        self.player = 0 # who's turn?
 
     def insert(self, column):
         i = len(self.grid[column])-1
         while i >= 0:
             if self.grid[column][i] == '.':
                 self.grid[column][i] = PLAYER_TOKENS[self.player]
-                self.player = (self.player+1)%2
+                self.player = (self.player+1)%2 # Toggle current player
                 break
             i -= 1
         pass
 
     def check_for_winner(self):
+
+        winning_strings = [token * 4 for token in PLAYER_TOKENS]
+        print winning_strings
+        
         # vertical
         lines = []
         for column in self.grid:
             lines += ["".join(map(str, column))]
+
         # horizontal
         for y in range(self.rows):
             lines += ["".join([self.grid[x][y] for x in range(self.cols)])]
-        print lines
-        # diagonal /
+
+ 
         # diagonal \
+        start_x = 0
+        for start_y in range(self.rows - 1):
+            line = ""
+            x, y = start_x, start_y
+            while 0 <= x < self.cols and 0 <= y < self.rows:
+                line += self.grid[x][y]
+                x, y = x+1, y+1
+            lines += [line]
 
+        start_y = 0
+        for start_x in range(self.cols - 1):
+            line = ""
+            x, y = start_x, start_y
+            while 0 <= x < self.cols and 0 <= y < self.rows:
+                line += self.grid[x][y]
+                x, y = x+1, y+1
+            lines += [line]
 
+        # diagonal /
+        start_x = self.cols - 1
+        for start_y in range(self.rows - 1):
+            line = ""
+            x, y = start_x, start_y
+            while 0 <= x < self.cols and 0 <= y < self.rows:
+                line += self.grid[x][y]
+                x, y = x-1, y+1
+            lines += [line]
 
+        start_y = self.rows - 1
+        for start_x in range(self.cols - 1):
+            line = ""
+            x, y = start_x, start_y
+            while 0 <= x < self.cols and 0 <= y < self.rows:
+                line += self.grid[x][y]
+                x, y = x+1, y-1
+            lines += [line]
+
+        print lines
+        # check if a winning string is in lines
+        # if winning_strings is in lines
+        # print the winner, end the game
 
     def __str__(self):
         string = '\n'
@@ -55,18 +91,14 @@ class Board(object):
         string += "\n" + "  ".join([str(i) for i in range(self.cols)])
         return string
 
-    def display(self):
-        print self
 
 board = Board()
-
 while True:
     print board
     try:
         column = int(raw_input("\nPlayer %s, choose a column: " %PLAYER_TOKENS[board.player]))
+        if 0 <= column <= board.cols: 
+            board.insert(column)
+            board.check_for_winner()
     except ValueError:
         print "Please specify a number [0-%i]" % board.cols
-    if 0 <= column <= board.cols: 
-        board.insert(column)
-        board.check_for_winner()
-
